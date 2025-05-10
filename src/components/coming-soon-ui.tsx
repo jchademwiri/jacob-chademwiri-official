10ca31bfba82008e25d0dc5f4ded87fe0266df4e
Jacob Chademwiri <jchademwiri@gmail.com>
2025-05-10 17:31:16 +0200

    fixed build errors

--- src/components/coming-soon-ui.tsx
+++ src/components/coming-soon-ui.tsx
@@ -36,7 +36,7 @@ export default function ModernDarkUI() {
               darkMode ? 'text-green-300' : 'text-green-700'
             } max-w-md mx-auto`}
           >
-            Something great is on the way. We're working hard to launch our
+            Something great is on the way. We&apos;re working hard to launch our
             site!
           </p>
         </div>
