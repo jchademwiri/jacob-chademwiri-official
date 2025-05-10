ad7da0bf5e174b5404f7ab309079ab978e403d05
Jacob Chademwiri <jchademwiri@gmail.com>
2025-05-10 16:21:03 +0200

    removed sign-up links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import { Button } from '@/components/ui/button';
-import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Button variant={'outline'}>Click Me</Button>
+        <h1>Site coming soon</h1>
+        <p>Stay tuned for updates!</p>
+
+        <Link href="mailto:hello@jacobc.co.za">Check Updates</Link>
       </main>
     </div>
   );
