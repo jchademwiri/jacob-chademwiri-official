4b1c84688b021e3beef6861a94cda6c738c42a0c
Jacob Chademwiri <jchademwiri@gmail.com>
2025-05-10 16:03:45 +0200

    setup supabase auth

--- src/app/protected/page.tsx
+++ src/app/protected/page.tsx
@@ -1,22 +1,24 @@
-import { redirect } from 'next/navigation'
+import { redirect } from 'next/navigation';
 
-import { LogoutButton } from '@/components/logout-button'
-import { createClient } from '@/lib/server'
+import { LogoutButton } from '@/components/logout-button';
+import { createClient } from '@/lib/server';
 
 export default async function ProtectedPage() {
-  const supabase = await createClient()
+  const supabase = await createClient();
 
-  const { data, error } = await supabase.auth.getUser()
+  const { data, error } = await supabase.auth.getUser();
   if (error || !data?.user) {
-    redirect('/auth/login')
+    redirect('/auth/login');
   }
 
   return (
-    <div className="flex h-svh w-full items-center justify-center gap-2">
+    <div className="flex h-svh w-full flex-col items-center justify-center gap-2">
       <p>
         Hello <span>{data.user.email}</span>
       </p>
+      <p>This is a Protected page</p>
+      <div>{data.user.new_phone} </div>
       <LogoutButton />
     </div>
-  )
+  );
 }
