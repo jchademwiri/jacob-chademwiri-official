4a94d59689817a1c3240e4360178def2e289474e
Jacob Chademwiri <jchademwiri@gmail.com>
2025-05-13 04:17:01 +0200

    layout and theme toggle

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -75,7 +75,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-background text-foreground system`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-background text-foreground system min-h-screen flex flex-col`}
       >
         <ThemeProvider
           attribute="class"
@@ -85,11 +85,9 @@ export default function RootLayout({
         >
           <Navbar />
 
-          <main className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
-            {children}
-          </main>
-          <ScrollToTop />
+          <main className="flex-1">{children}</main>
           <Footer />
+          <ScrollToTop />
         </ThemeProvider>
         <SpeedInsights />
         <Analytics />
