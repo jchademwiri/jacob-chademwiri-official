b434fcc4f3015860879e71f0d928ef8604f7b648
Jacob Chademwiri <jchademwiri@gmail.com>
2025-05-28 08:09:07 +0200

    remove image configuration from next.config.ts; add comprehensive portfolio website development plan

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  images: {
-    remotePatterns: [new URL('https://images.pexels.com/photos/39284/**')],
-  },
 };
 
 export default nextConfig;
