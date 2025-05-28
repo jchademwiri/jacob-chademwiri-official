37f89637564fd507419b39c9fd582921f13fcbb4
Jacob Chademwiri <jchademwiri@gmail.com>
2025-05-28 16:13:32 +0200

    coming soon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,19 @@
 import ComingSoon from '@/components/coming-soon';
-import {
-  CurrentEmployment,
-  FeaturesSection,
-  HeroSection,
-  ProjectsSection,
-} from '@/components/sections';
+// import {
+//   CurrentEmployment,
+//   FeaturesSection,
+//   HeroSection,
+//   ProjectsSection,
+// } from '@/components/sections';
 
 export default function Home() {
   return (
     <>
       <ComingSoon />
-      <CurrentEmployment />
+      {/* <CurrentEmployment />
       <HeroSection />
       <FeaturesSection />
-      <ProjectsSection />
+      <ProjectsSection /> */}
     </>
   );
 }
